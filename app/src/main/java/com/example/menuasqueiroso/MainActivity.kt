d753d699e018a4497beffd48aec9325b682cff21
Halanna Lima <104534961+lanninha@users.noreply.github.com>
2024-07-22 17:44:47 -0300

    Menu

---
+++ app/src/main/java/com/example/menuasqueiroso/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.menuasqueiroso
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.main)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+        val menu = findViewById<ImageView>(R.id.menu_lua)
+
+        menu.setOnClickListener {
+           drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        navView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            drawerLayout.closeDrawers()
+
+            when (menuItem.itemId) {
+                R.id.settings -> {
+                    val intent = Intent (this,SettingsActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            true
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+        }
+    }
+}
